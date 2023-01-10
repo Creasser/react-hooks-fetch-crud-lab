3581b654eb55037ae518cf454e10c585a4739844
Blake Creasser <blakecreasser@aggienetwork.com>
2023-01-10 13:36:15 -0600

    commit tree 55551346c918a6b4fd79b2d7e29e6c12fb39a529

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,22 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState(null)
+
+  function handleNewQuestion(newQuestion){
+    setQuestions([...questions, newQuestion])
+  }
+
+  function handleDeletedQuestion(deletedQuestion){
+    const newQuestionList = questions.filter((question => question.id !== deletedQuestion.id))
+    console.log(newQuestionList)
+    setQuestions(newQuestionList)
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm setQuestions={handleNewQuestion} /> : <QuestionList questions={questions} setQuestions={setQuestions} handleDelete={handleDeletedQuestion} />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questions, setQuestions, handleDelete}) {
+  
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+    .then(r => r.json())
+    .then (data => setQuestions(data))
+  })
+
+  if(!questions){
+    return <div>Loading...</div>
+  }
+
+  function handleUpdateQuestion(updatedQuestion){
+    const updatedQuestions = questions.map((question) => {
+      if (question.id === updatedQuestion.id){
+        return updatedQuestion
+      }else{
+        return question
+      }
+    })
+    setQuestions(updatedQuestions)
+  }
+
+  const questionElements = questions.map((question) => {
+    return <QuestionItem key={question.id} question={question} deleteQ={handleDelete} handleUpdateQuestion={handleUpdateQuestion} />
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionElements}</ul>
     </section>
   );
 }
